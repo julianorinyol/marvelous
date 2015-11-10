bb5bf546c0933ebaa70214f14a0bbb2971d0dea3
Julian Orinyol <julianorinyol@gmail.com>
2015-11-10 10:30:44 -0800

    added hardcoded lists to scope, and ng-repeats to iterate over them in markup

--- app.js
+++ app.js
@@ -20,4 +20,17 @@ function($scope){
   $scope.title = 'Marvelous!';
   $scope.column1name = 'COLUMN 1';
   $scope.column2name = 'COLUMN 2';
+  $scope.items = [
+    'ITEMa',
+    'ITEMb',
+    'ITEMc',
+    'ITEMd',
+    'ITEMe'
+  ];
+  $scope.otherItems = [
+  	'ITEM1'  ,
+  	'ITEM2',
+  	'ITEM3',
+  	'ITEM4'
+  ];
 }]);
